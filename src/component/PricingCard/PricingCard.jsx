ff5d0d94afe197d9a725811e2011d24f769d1b33
Fatema <fatematz486@gmail.com>
2026-03-22 12:08:47 +0600

    Update PricingCard.jsx

--- src/component/PricingCard/PricingCard.jsx
+++ src/component/PricingCard/PricingCard.jsx
@@ -8,7 +8,7 @@ const PricingCard=({pricing}) => {
             {
                 <div className='bg-amber-300 border m-4 rounded-[20px] p-5'>
                     { popular && 
-                    <span  className="bg-purple-800 text-white py-1 px-3  rounded-3xl absolute -mt-9 ml-75 ">most popular</span>
+                    <span  className="bg-purple-800 text-white py-1 px-4  rounded-3xl absolute -mt-9 ml-50 ">most popular</span>
                         }
                     <h1 className="text-3xl md:text-6xl font-bold mt-4 mb-4"> {name} </h1>
                     <h2 className="text-2xl md:text-4xl  font-bold mb-4">${price}/month</h2>
